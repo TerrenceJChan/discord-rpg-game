bbdd396420d279d394499192bd6eddabbfe99eb1
TerrenceJChan <terrencechan.writer@gmail.com>
2021-06-02 18:21:37 -0400

    Bot simulates a basic fight with some random number generation

--- index.js
+++ index.js
@@ -28,16 +28,55 @@ let player = {
     "def": 7
 }
 
+const hunt = () => {
+    fightState = true;
+}
+
+const attack = () => {
+    const random20 = () => {
+        return 1 + ((Math.floor(Math.random() * 41) - 20) / 100);
+    }
+
+    let enemyDamage = Math.floor((player.atk * random20()) - (enemy.def * random20()));
+    console.log(enemyDamage);
+    if (enemyDamage < 0) {
+        enemyDamage = 0;
+    }
+
+    let playerDamage = Math.floor((enemy.atk * random20()) - (player.def * random20()));
+    console.log(playerDamage);
+    if (playerDamage < 0) {
+        playerDamage = 0;
+    }
+
+    enemy.hp =- enemyDamage;
+    player.hp =- playerDamage;
+
+    return(`The adventurer slashes the dragon for ${enemyDamage} damage! The dragon retaliates and deals ${playerDamage} damage!`)
+}
+
 bot.on("message", msg => {
-    if (msg.content.charAt(0) === COMMAND) {
-        switch (msg.content) {
-            case COMMAND + "test":
-                msg.channel.send('test');
-                break;
-            case COMMAND + "greet":
-                msg.channel.send('Hello!');
-                break;
+    if (fightState === false) {
+        if (msg.content.charAt(0) === COMMAND) {
+            switch (msg.content) {
+                case COMMAND + "hunt":
+                    msg.channel.send("You encounter a dragon! You assume a battle stance as it directs its furious attention in your direction.");
+                    hunt();
+                    break;
+                case COMMAND + "greet":
+                    msg.channel.send("Hello!");
+                    break;
+            }
+            // msg.reply("noot noot");
         }
-        // msg.reply("noot noot");
+    } else {
+        if (msg.content.charAt(0) === COMMAND) {
+            switch (msg.content) {
+                case COMMAND + "attack":
+                    msg.channel.send(attack());
+                    break;
+            }
+            // msg.reply("noot noot");
+        }
     }
 });
