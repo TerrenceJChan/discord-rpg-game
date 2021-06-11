5e46dc59ca75d8563b6c83de9566ecfa31f4f470
Terrence Chan <terrencechan.writer@gmail.com>
2021-06-10 20:11:08 -0400

    Add player and weapon classes

---
+++ player/Player.js
@@ -0,0 +1,9 @@
+export default class Player {
+    constructor() {
+        this.name = 'Hero';
+        this.hp = 100;
+        this.atk = 50;
+        this.def = 7;
+        this.skills;
+    }
+}
