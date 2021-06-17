0ce9a6a57ad25ad2471be3e713c041248795dbae
Cameron Eldridge <cameldridge@gmail.com>
2021-06-16 20:31:32 -0400

    Attempt components, requires discord.js to be updated

--- src/commands/greet.js
+++ src/commands/greet.js
@@ -1,1 +1,20 @@
-export const greet = () => 'Hello';
+import { MessageButton, MessageActionRow } from 'discord.js';
+
+export const greet = ({ msg }) => {
+  msg.channel.send('Hello', {
+    components: [new MessageActionRow()
+      .addComponents(
+        new MessageButton()
+          .setCustomID('fish')
+          .setEmoji('fish')
+          .setLabel('Fish?')
+          .setStyle('PRIMARY'),
+        new MessageButton()
+          .setCustomID('monster')
+          .setEmoji('monster')
+          .setLabel('Monster!')
+          .setStyle('DANGER'),
+      )],
+  });
+  return 'You have been greeted.';
+};
