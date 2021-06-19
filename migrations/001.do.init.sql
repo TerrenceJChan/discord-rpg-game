d1780e12178255cc4433098b5030889a8fb16878
Terrence Chan <terrencechan.writer@gmail.com>
2021-06-19 19:24:45 -0400

    Add more equipment related tables

--- migrations/001.do.init.sql
+++ migrations/001.do.init.sql
@@ -22,18 +22,23 @@ CREATE TABLE hunt (
     enemy_id INT NOT NULL references enemies(id) ON DELETE CASCADE,
     PRIMARY KEY(character_id, enemy_id)
 );
--- BlEH
 CREATE TYPE enum_material AS ENUM('Dragon Talon', 'Dragon Hide', 'Dragon Fang');
 CREATE TABLE materials (
+    type enum_material NOT NULL,
     quantity (int),
     PRIMARY KEY(player_id, material_id)
 );
+CREATE TYPE enum_geartype AS ENUM('DragonsBite');
 CREATE TABLE equipment (
     id SERIAL PRIMARY KEY,
     player_id INT NOT NULL references players(id) ON DELETE CASCADE,
-    equipment_id INT NOT NULL references
+    type enum_geartype NOT NULL;
 );
--- BLEH
+-- CREATE TYPE enum_mod AS ENUM('PhysAtk', 'FireAtk', 'IceAtk', 'ShockAtk', 'PhysDef', 'FireDef', 'IceDef', 'ShockDef');
+CREATE TABLE equipment_mods (
+    mods JSONB,
+    PRIMARY KEY(player_id, equipment_id),
+);
 ALTER TABLE players
 ADD COLUMN active_character INT REFERENCES characters(id) ON DELETE
 SET NULL;
