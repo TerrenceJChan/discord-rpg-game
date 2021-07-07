835ed785b886efaf18ca6a3fa326b230572d1394
Terrence Chan <terrencechan.writer@gmail.com>
2021-07-06 22:09:35 -0400

    Fix 004

--- migrations/004.do.inventory-update.sql
+++ migrations/004.do.inventory-update.sql
@@ -1,20 +1,13 @@
 CREATE TYPE enum_material AS ENUM('Dragon Talon', 'Dragon Hide', 'Dragon Fang');
 CREATE TABLE materials (
-    type enum_material NOT NULL,
+    material_type enum_material NOT NULL,
     quantity INT NOT NULL,
-    PRIMARY KEY(player_id, material_id)
+    player_id VARCHAR(20) NOT NULL references players(id) ON DELETE CASCADE,
+    PRIMARY KEY(player_id, material_type)
 );
 CREATE TYPE enum_geartype AS ENUM('DragonsBite');
 CREATE TABLE equipment (
     id SERIAL PRIMARY KEY,
-    player_id INT NOT NULL references players(id) ON DELETE CASCADE,
-    type enum_geartype NOT NULL
+    player_id VARCHAR(20) NOT NULL references players(id) ON DELETE CASCADE,
+    mods JSONB NOT NULL DEFAULT '[]' CHECK (jsonb_typeof(mods) = 'array')
 );
-CREATE TABLE equipment_mods (
-    id SERIAL PRIMARY KEY,
-    equipment_id INT NOT NULL REFERENCES equipment (id) ON DELETE CASCADE,
-    mods JSONB NOT NULL
-);
-ALTER TABLE players
-ADD COLUMN active_character INT REFERENCES characters(id) ON DELETE
-SET NULL;
--- migrations/004.undo.inventory-update.sql
+++ migrations/004.undo.inventory-update.sql
@@ -1,6 +1,4 @@
-ALTER TABLE players DROP COLUMN active_character;
-DROP TABLE equipment mods;
 DROP TABLE equipment;
-DROP TYPE enum_geartype;
 DROP TABLE materials;
+DROP TYPE enum_geartype;
 DROP TYPE enum_material;
